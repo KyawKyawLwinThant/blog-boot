c92a1647c264b50829bd697b8ebc522a42bf7774
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2019-11-26 14:55:30 +0630

    second commit

--- src/main/java/com/example/blogboot/BlogBootApplication.java
+++ src/main/java/com/example/blogboot/BlogBootApplication.java
@@ -8,6 +8,8 @@ public class BlogBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogBootApplication.class, args);
+
+        System.out.println("Hello World.");
     }
 
 }
