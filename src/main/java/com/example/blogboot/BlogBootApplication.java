18befdaa7ac436382839c0d1142ad39d43072c0c
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2019-11-26 13:42:01 +0630

    first commit

---
+++ src/main/java/com/example/blogboot/BlogBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBootApplication.class, args);
+    }
+
+}
