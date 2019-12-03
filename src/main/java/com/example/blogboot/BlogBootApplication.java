65e2f57217ee4ad6b1e1a6250bf0395c48d1007d
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2019-12-03 15:31:05 +0630

    post form

--- src/main/java/com/example/blogboot/BlogBootApplication.java
+++ src/main/java/com/example/blogboot/BlogBootApplication.java
@@ -9,8 +9,7 @@ public class BlogBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogBootApplication.class, args);
 
-        System.out.println("Hello World.");
-        System.out.println("Hello World.");        
+
     }
 
 }
---
+++ src/main/java/com/example/blogboot/controller/PostController.java
@@ -0,0 +1,60 @@
+package com.example.blogboot.controller;
+
+import com.example.blogboot.domain.Posts;
+import com.example.blogboot.service.AuthorService;
+import com.example.blogboot.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class PostController {
+    private final AuthorService authorService;
+    private final PostService postService;
+
+    public PostController(AuthorService authorService, PostService postService) {
+        this.authorService = authorService;
+        this.postService = postService;
+    }
+
+    @GetMapping("/post")
+    public String createPost(Model model){
+        model.addAttribute("post",new Posts());
+        model.addAttribute("authors",authorService.findAll());
+        return "admin/postForm";
+    }
+    @PostMapping("/post")
+    public String processPost(@Valid Posts post, RedirectAttributes redirectAttributes,
+                              Model model, BindingResult result){
+        if(result.hasErrors()){
+            model.addAttribute("authors",authorService.findAll());
+            return "admin/postForm";
+        }
+
+        postService.create(post);
+        redirectAttributes.addFlashAttribute("success",true);
+        return "redirect:/posts";
+    }
+    @GetMapping("/posts")
+    public  String showAllPosts(Model model){
+
+        model.addAttribute("posts",postService.findAll());
+        model.addAttribute("success"
+                ,model.containsAttribute("success"));
+        return "admin/posts";
+    }
+
+    @ModelAttribute(name = "categories")
+    public List<String> categories(){
+        return Arrays.asList("Horror","Tragedy","Si-Fi","Romance","Travel");
+    }
+}
